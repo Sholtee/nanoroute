6327279c62b151e3c77d2f21d303b6df23106b8e
denes.solti <sodnaatx@gmail.com>
2026-04-29 09:48:24 +0200

    [#48] remove RuntimeFeature polyfill from netstandard2.1+ builds

--- Src/NanoRoute/Private/RoutePattern/ValueParserDefinition.cs
+++ Src/NanoRoute/Private/RoutePattern/ValueParserDefinition.cs
@@ -6,6 +6,7 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using System.Text.RegularExpressions;
 
 namespace NanoRoute.Internals
