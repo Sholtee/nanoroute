26dbaf9440d1b508c4745c2b46a029ace03b92f1
denes.solti <sodnaatx@gmail.com>
2026-05-17 12:20:34 +0200

    [#84] break down RouterTests.cs into multiple files

---
+++ Tests/NanoRoute.Tests/RouterTests.Cancellation.cs
@@ -0,0 +1,109 @@
+/********************************************************************************
+* RouterTests.Cancellation.cs                                                   *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Moq;
+using NUnit.Framework;
+
+namespace NanoRoute.Tests
+{
+    using Properties;
+
+    internal sealed partial class RouterTests
+    {
+        [Test]
+        public void Handle_ShouldBeNullChecked()
+        {
+            ArgumentNullException ex = Assert.ThrowsAsync<ArgumentNullException>(() => _routerBuilder.CreateRouter().Handle(_request, null!))!;
+            Assert.That(ex.ParamName, Is.EqualTo("services"));
+        }
+
+        [Test]
+        public void Handle_CanBeCancelled()
+        {
+            Mock<RequestHandlerDelegate> mockHandler = new(MockBehavior.Strict);
+            mockHandler
+                .Setup(h => h.Invoke(It.Is<RequestContext>(c => c.Request == _request), It.IsAny<CallNextHandlerDelegate>()))
+                .Returns<RequestContext, CallNextHandlerDelegate>((context, _) =>
+                {
+                    context.Cancellation.ThrowIfCancellationRequested();
+                    return Task.FromResult(s_response);
+                });
+
+            TestRouter router = _routerBuilder
+                .AddHandler("GET", "/", mockHandler.Object)
+                .CreateRouter();
+
+            _request.RequestUri = new Uri("https://www.exmaple.com/");
+
+            using CancellationTokenSource cts = new();
+            cts.Cancel();
+
+            Assert.That(async () => await router.Handle(_request, new Mock<IServiceProvider>(MockBehavior.Loose).Object, cts.Token), Throws.InstanceOf<OperationCanceledException>());
+            mockHandler.Verify(h => h.Invoke(It.IsAny<RequestContext>(), It.IsAny<CallNextHandlerDelegate>()), Times.Never);
+        }
+
+        [Test]
+        public void Handle_ShouldPropagateCancellationDuringHandlerExecution()
+        {
+            using CancellationTokenSource cancellation = new(TimeSpan.FromMilliseconds(50));
+
+            Mock<RequestHandlerDelegate> mockHandler = new(MockBehavior.Strict);
+            mockHandler
+                .Setup(h => h.Invoke(It.Is<RequestContext>(c => c.Request == _request && c.Cancellation.CanBeCanceled), It.IsAny<CallNextHandlerDelegate>()))
+                .Returns<RequestContext, CallNextHandlerDelegate>(async (context, _) =>
+                {
+                    await Task.Delay(Timeout.InfiniteTimeSpan, context.Cancellation);
+                    return s_response;
+                });
+
+            TestRouter router = _routerBuilder
+                .AddHandler("GET", "/", mockHandler.Object)
+                .CreateRouter();
+
+            _request.RequestUri = new Uri("https://www.exmaple.com/");
+
+            Assert.That(async () => await router.Handle(_request, new Mock<IServiceProvider>(MockBehavior.Loose).Object, cancellation.Token), Throws.InstanceOf<OperationCanceledException>());
+            mockHandler.Verify(h => h.Invoke(It.IsAny<RequestContext>(), It.IsAny<CallNextHandlerDelegate>()), Times.Once);
+        }
+
+        [Test]
+        public async Task Handle_ShouldExposeTheCallerCancellationToken()
+        {
+            using CancellationTokenSource cts = new();
+
+            Mock<RequestHandlerDelegate> mockHandler = new(MockBehavior.Strict);
+            mockHandler
+                .Setup(h => h.Invoke(It.Is<RequestContext>(c => c.Request == _request && c.Cancellation.Equals(cts.Token)), It.IsAny<CallNextHandlerDelegate>()))
+                .ReturnsAsync(s_response);
+
+            TestRouter router = _routerBuilder
+                .AddHandler("GET", "/", mockHandler.Object)
+                .CreateRouter();
+
+            _request.RequestUri = new Uri("https://www.exmaple.com/");
+
+            Assert.That(await router.Handle(_request, new Mock<IServiceProvider>(MockBehavior.Loose).Object, cts.Token), Is.EqualTo(s_response));
+            mockHandler.Verify(h => h.Invoke(It.IsAny<RequestContext>(), It.IsAny<CallNextHandlerDelegate>()), Times.Once);
+        }
+
+        [Test]
+        public void Handle_ShouldRejectUnsupportedVerbs()
+        {
+            TestRouter router = _routerBuilder.CreateRouter();
+
+            _request.Method = new HttpMethod("BREW");
+            _request.RequestUri = new Uri("https://www.exmaple.com/");
+
+            ArgumentException ex = Assert.ThrowsAsync<ArgumentException>(() => router.Handle(_request, new Mock<IServiceProvider>(MockBehavior.Loose).Object))!;
+            Assert.That(ex.ParamName, Is.EqualTo("request"));
+            Assert.That(ex.Message, Does.Contain(string.Format(Resources.Culture, Resources.ERR_INVALID_VERB, "BREW")));
+        }
+    }
+}
---
+++ Tests/NanoRoute.Tests/RouterTests.Diagnostics.cs
@@ -0,0 +1,82 @@
+/********************************************************************************
+* RouterTests.Diagnostics.cs                                                    *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using System;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Moq;
+using NUnit.Framework;
+
+namespace NanoRoute.Tests
+{
+    using Properties;
+
+    internal sealed partial class RouterTests
+    {
+        [Test]
+        public async Task Handle_ShouldLogTheRequestLifecycle()
+        {
+            TestRouter router = _routerBuilder
+                .AddHandler("GET", "/path/to/somewhere/", async (_, _) => s_response)
+                .CreateRouter();
+
+            _request.RequestUri = new Uri("https://www.exmaple.com/path/to/somewhere");
+
+            HttpResponseMessage response = await router.Handle(_request, new Mock<IServiceProvider>(MockBehavior.Loose).Object);
+
+            Assert.That(response, Is.EqualTo(s_response));
+            Assert.That(SpinWait.SpinUntil(() => _debugEventListener.Events.Count >= 2, 1000), Is.True);
+
+            EventWrittenEventArgs
+                requestStarted = _debugEventListener.Events.Single(e => e.EventName == "RequestProcessingStarted"),
+                matchingHandler = _debugEventListener.Events.Single(e => e.EventName == "MatchingHandler");
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(requestStarted.Level, Is.EqualTo(EventLevel.Informational));
+                Assert.That(requestStarted.PayloadNames, Is.EquivalentTo(new[] { "RequestUri", "Verb" }));
+                Assert.That(requestStarted.Payload, Is.EquivalentTo(new object?[] { "https://www.exmaple.com/path/to/somewhere", HttpMethod.Get.Method }));
+
+                Assert.That(matchingHandler.Level, Is.EqualTo(EventLevel.Informational));
+                Assert.That(matchingHandler.PayloadNames, Is.EquivalentTo(new[] { "RequestUri", "Verb", "Pattern", "ParameterCount" }));
+                Assert.That(matchingHandler.Payload, Is.EquivalentTo(new object?[] { "https://www.exmaple.com/path/to/somewhere", HttpMethod.Get.Method, "/path/to/somewhere/", 0 }));
+            });
+        }
+
+        [Test]
+        public void Handle_ShouldLogWhenNoHandlerMatches()
+        {
+            TestRouter router = _routerBuilder.CreateRouter();
+
+            _request.RequestUri = new Uri("https://www.exmaple.com/path/to/nowhere");
+
+            HttpRequestException ex = Assert.ThrowsAsync<HttpRequestException>(() => router.Handle(_request, new Mock<IServiceProvider>(MockBehavior.Loose).Object))!;
+
+            Assert.That(ex.Message, Is.EqualTo(Resources.ERR_NOT_FOUND));
+            Assert.That(ex.Data["StatusCode"], Is.EqualTo(HttpStatusCode.NotFound));
+            Assert.That(SpinWait.SpinUntil(() => _debugEventListener.Events.Count >= 2, 1000), Is.True);
+
+            EventWrittenEventArgs
+                requestStarted = _debugEventListener.Events.Single(e => e.EventName == "RequestProcessingStarted"),
+                noMatchingHandler = _debugEventListener.Events.Single(e => e.EventName == "NoMatchingHandler");
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(requestStarted.Level, Is.EqualTo(EventLevel.Informational));
+                Assert.That(requestStarted.PayloadNames, Is.EquivalentTo(new[] { "RequestUri", "Verb" }));
+                Assert.That(requestStarted.Payload, Is.EquivalentTo(new object?[] { "https://www.exmaple.com/path/to/nowhere", HttpMethod.Get.Method }));
+
+                Assert.That(noMatchingHandler.Level, Is.EqualTo(EventLevel.Informational));
+                Assert.That(noMatchingHandler.PayloadNames, Is.EquivalentTo(new[] { "RequestUri", "Verb" }));
+                Assert.That(noMatchingHandler.Payload, Is.EquivalentTo(new object?[] { "https://www.exmaple.com/path/to/nowhere", HttpMethod.Get.Method }));
+            });
+        }
+    }
+}
