4fada94df80b32d3a168f414b05dd90f89accc1e
denes.solti <sodnaatx@gmail.com>
2026-05-29 06:35:46 +0200

    [#102] add RouteNodeGraphWalkingBenchmarks

---
+++ Tests/NanoRoute.Perf/RouteNodeGraphWalkingBenchmarks.cs
@@ -0,0 +1,63 @@
+/********************************************************************************
+* RouteNodeGraphWalkingBenchmarks.cs                                            *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using BenchmarkDotNet.Attributes;
+
+namespace NanoRoute.Perf
+{
+    using Internals;
+
+    [MemoryDiagnoser]
+    public class RouteNodeGraphWalkingBenchmarks
+    {
+        private const string LiteralRoutePattern = "/api/v1/users/42/orders/7/items/3/details/";
+
+        private static readonly Task<HttpResponseMessage> s_responseTask = Task.FromResult(new HttpResponseMessage());
+
+        private static readonly RequestHandlerDelegate s_handler = static (_, _) => s_responseTask;
+
+        private RouteNode _root = null!;
+
+        [GlobalSetup]
+        public void Setup() => _root = TestRouter
+            .CreateBuilder()
+            .AddHandler("GET", LiteralRoutePattern, s_handler)
+            .CreateSnapshot();
+
+        [Benchmark(Baseline = true)]
+        public object WalkSingleBranchGraph()
+        {
+            RouteNode node = _root;
+
+            while (node.SingleBranch is KeyValuePair<ReadOnlyMemory<char>, RouteNode> literalBranch)
+                node = literalBranch.Value;
+
+            return node;
+        }
+
+        [Benchmark]
+        public object MatchLiteralSegmentsAndWalkGraph()
+        {
+            RouteNode node = _root;
+
+            for (DelimitedSegment segment = new(LiteralRoutePattern.AsMemory(), '/'); segment.MoveNext();)
+            {
+                if (node.SingleBranch is not KeyValuePair<ReadOnlyMemory<char>, RouteNode> literalBranch || !ReadOnlyMemoryCharComparer.Instance.Equals(literalBranch.Key, segment.Current))
+                    throw new InvalidOperationException($"Failed to match segment '{segment.Current}'.");
+
+                node = literalBranch.Value;
+            }
+
+            return node;
+        }
+
+        private sealed class TestRouter(RouterBuilder<TestRouter, RouterConfig> builder) : Router<TestRouter, RouterConfig>(builder);
+    }
+}
