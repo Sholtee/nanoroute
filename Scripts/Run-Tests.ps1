8974578cea42bb2f9fcabb8a6ad037eba6c80fc5
denes.solti <sodnaatx@gmail.com>
2026-06-21 16:12:51 +0200

    [#112] find test projects by name

--- Scripts/Run-Tests.ps1
+++ Scripts/Run-Tests.ps1
@@ -17,23 +17,30 @@ foreach ($dir in @("Artifacts"; "BIN"; "OBJ")) {
   Remove-Item (Join-Path $ROOT $dir)  -Recurse -Force -ErrorAction SilentlyContinue
 }
 
-function Run-Tests([Parameter(Position=0, Mandatory=$true)][string] $csproj, [Parameter(Position=1, Mandatory=$true)][string] $display) {
-  Write-Host "`n-------------------------------------------$display-------------------------------------------"
-  dotnet build-server shutdown
+function Run-Tests([Parameter(Position=0, Mandatory=$true)][System.IO.FileInfo] $project) {
+  $shortName = [System.IO.Path]::GetFileNameWithoutExtension($project.Name)
 
-  $shortName = [System.IO.Path]::GetFileNameWithoutExtension($csproj)
-  $csproj = (Get-ChildItem -Path (Join-Path $ROOT "Tests") -Recurse -Filter $csproj).FullName
+  Write-Host "`n-------------------------------------------$shortName tests (with coverage)-------------------------------------------"
+  dotnet build-server shutdown
 
   dotnet tool run dotnet-coverage collect `
     --settings ([System.IO.Path]::Combine($ROOT, "Tests", "CoverageSettings.xml")) `
     --output (Join-Path $ARTIFACTS "$shortName.Coverage.xml") `
-    "dotnet test $csproj --configuration:Debug --logger:`"junit;LogFilePath=$(Join-Path $ARTIFACTS "$shortName.Results.xml")`""
+    "dotnet test `"$($project.FullName)`" --configuration:Debug --logger:`"junit;LogFilePath=$(Join-Path $ARTIFACTS "$shortName.Results.xml")`""
 
   if (-not $?) { throw "Test session failed" }
 }
 
-Run-Tests 'NanoRoute.Tests.csproj' 'Regular tests (with coverage)'
-Run-Tests 'NanoRoute.AwsLambda.Tests.csproj' 'AWS Lambda tests (with coverage)'
+$testProjects = @(
+  Get-ChildItem -Path (Join-Path $ROOT "Tests") -Recurse -File -Filter "*.csproj" |
+    Where-Object { $_.BaseName -match "\.Tests?$" } |
+    Sort-Object -Property FullName
+)
+if ($testProjects.Count -eq 0) { throw "No test projects found" }
+
+foreach ($testProject in $testProjects) {
+  Run-Tests $testProject
+}
 
 dotnet tool run reportgenerator `
   -reports:((Get-ChildItem -Path $ARTIFACTS -File -Filter '*.Coverage.xml' | Select-Object -ExpandProperty FullName) -join ';') `
